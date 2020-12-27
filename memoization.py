7dc86a70704eeccf454b50f5f063b50535e2cd1c
cosmasadri <32033170+cosmasadri@users.noreply.github.com>
2020-12-27 02:22:44 +0100

    🎉 initial commit

---
+++ memoization.py
@@ -0,0 +1,34 @@
+from threading import Timer
+
+
+def remove_key(key, cache_dict):
+    del cache_dict[key]
+
+def memoize(func, timeout, resolver=None):
+    cache = dict()
+    
+    if type(timeout) != int and type(timeout) != float:
+        raise TypeError("timeout must be integer or float")
+    else:
+        # converting from milliseconds into seconds
+        timeout = timeout/1000
+    
+    if not callable(func):
+        raise TypeError("func must be a function")
+    
+    if resolver is not None and not callable(resolver):
+        raise TypeError("resolver must be a function")
+    
+    def memoized_func(*args):
+        cache_key = str(args) if resolver is None else resolver(*args)
+        
+        if cache_key in cache:
+            return cache[cache_key]
+        
+        result = func(*args)
+        cache[cache_key] = result
+        
+        Timer(timeout, remove_key, (cache_key, cache)).start()
+        return result
+
+    return memoized_func
